dbae659fced988f0dd0cca11d4eb71e75afbda8f
Shefali Jain <114603553+shefalijain1311@users.noreply.github.com>
2024-07-12 10:21:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Root to leaf path sum/root-to-leaf-path-sum.java
@@ -0,0 +1,144 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+    public static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            int sum = Integer.parseInt(br.readLine().trim());
+            Solution tr = new Solution();
+            if (tr.hasPathSum(root, sum)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+// A Binary Tree node
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+
+
+/*
+// A Binary Tree node
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+class Solution {
+    boolean help(Node root,int target)
+    {
+        if(root.left==null && root.right==null)
+        {
+            if(root.data==target)
+            return true;
+            return false;
+        }
+        boolean left=false;
+        if(root.left!=null)
+        left=help(root.left,target-root.data);
+        boolean right=false;
+        if(root.right!=null)
+        right=help(root.right,target-root.data);
+        return left || right;
+    }/*you are required to complete this function */
+    boolean hasPathSum(Node root, int target) {
+       return help(root,target);
+    }
+}
