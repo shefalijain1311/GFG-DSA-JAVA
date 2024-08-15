a34080b207245eaee9704f35e1a5ca9e4f05e492
Shefali Jain <114603553+shefalijain1311@users.noreply.github.com>
2024-08-15 23:05:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GfG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data);
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+            Solution obj = new Solution();
+            head = obj.addOne(head);
+            printList(head);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node addOne(Node head) {
+        // code here.
+           Node head1=reverse(head);
+        Node newHead=head1;
+        int carry=0;
+        while(head1!=null){
+            int val=head1.data+1;
+            if(val<=9){
+                head1.data=val;
+                carry=0;
+                break;
+            }else{
+                head1.data=0;
+                carry=1;
+            }
+            head1=head1.next;
+        }
+        if(carry==1){
+            Node newNode=new Node(1);
+            newNode.next=newHead;
+            return newNode;
+        }
+        else{
+            Node head2=reverse(newHead);
+            return head2;
+    }
+}
+public Node reverse(Node head){
+        Node curr=head;
+        Node prev=null;
+        while(curr!=null){
+            Node temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+}
+}
